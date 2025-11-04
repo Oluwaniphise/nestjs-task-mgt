ea70af16220975c66cef3a0179018c46eb80e29b
Oduyale Enoch <enoch.oduyale@reventtechnologies.com>
2025-11-04 02:35:00 +0100

    feat(tasks): implement user-task association and add user decorator for task creation

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './transform.interceptor';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -5,9 +5,9 @@ import { TasksRepository } from './tasks.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './task.entity';
 import { AuthModule } from 'src/auth/auth.module';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [TypeOrmModule.forFeature([Task]), AuthModule],
+  imports: [TypeOrmModule.forFeature([Task]), AuthModule, ConfigModule],
   controllers: [TasksController],
   providers: [TasksService, TasksRepository],
 })
